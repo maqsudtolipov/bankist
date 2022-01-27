5bfcf26df38d4c6e74164ef973df022aa4337b22
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-27 17:48:35 +0500

    Implement chaining methods

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>
   `;
 
@@ -84,9 +84,32 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -329,8 +352,15 @@ console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 */
 
 const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
 const totalDepositsUSD = movements
   .filter(mov => mov > 0)
-  .map(mov => mov * 1.1)
+  // .map(mov => mov * 1.1)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * 1.1;
+  })
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
