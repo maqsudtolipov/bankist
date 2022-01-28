13fae0e23bfbb6cad82aae0312e967582be02ca0
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-28 10:30:52 +0500

    Find method

--- script.js
+++ script.js
@@ -103,7 +103,6 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -351,6 +350,10 @@ console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 */
 
+/////////////////////////////////////////////////
+// Chaining methods
+
+/*
 const eurToUsd = 1.1;
 console.log(movements);
 
@@ -364,3 +367,39 @@ const totalDepositsUSD = movements
   })
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
+
+/////////////////////////////////////////////////
+// Coding challenge #3
+
+/*
+const calcAverageHumanAge2 = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(dog => dog >= 18);
+
+  const average = adults.reduce(
+    (acc, dog, i, arr) => acc + dog / arr.length,
+    0
+  );
+
+  return average;
+};
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(dog => dog >= 18)
+    .reduce((acc, dog, i, arr) => acc + dog / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis')
+console.log(account);
