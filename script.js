a7a9587bde5c992e7442b4e5dd0f3c0820fabfd9
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-02-03 22:17:40 +0500

    Learn dates

--- script.js
+++ script.js
@@ -418,3 +418,43 @@ console.log(huge + ' is REALLY big!!!');
 console.log(11n / 3n);
 console.log(30 / 3);
 */
+
+/////////////////////////////////////////////////
+// Dates
+
+/*
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Feb 03 2022 21:56:02'));
+console.log(new Date('March 6, 2005'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 36));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.getMilliseconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142238980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
