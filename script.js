0b7b79ed6204bfa09da9f2280a11de07903adb28
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-02-03 21:52:21 +0500

    Learn BigInt, remainder and numeric separations

--- script.js
+++ script.js
@@ -333,3 +333,88 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
 */
+
+/////////////////////////////////////////////////
+// Math and Rounding
+
+/*
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = '#eee';
+    if (i % 3 === 0) row.style.backgroundColor = '#ddd';
+  });
+});
+*/
+
+/////////////////////////////////////////////////
+// Numeric separators
+
+/*
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
+*/
+
+/////////////////////////////////////////////////
+// BigInts
+
+/*
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(64687465046084684068046549018748n);
+console.log(BigInt(646874658748));
+
+// Operations
+console.log(31469840351684654084648463436416002345n + 100000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 6546546546546545605416n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == 20);
+
+console.log(huge + ' is REALLY big!!!');
+
+// Division
+console.log(11n / 3n);
+console.log(30 / 3);
+*/
